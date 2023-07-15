2900ded51ad9df98c984adbbc528c6bb10506049
Edwin Garcia <edwinprogramando@gmail.com>
2023-07-15 14:43:57 -0500

    get currencies from api

--- pkg/requester/requester.go
+++ pkg/requester/requester.go
@@ -1,6 +1,7 @@
 package requester
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
@@ -9,28 +10,51 @@ import (
 	"github.com/edwintrumpet/prueba-tecnica-boletia/config"
 )
 
+type response struct {
+	Meta struct {
+		LastUpdatedAt time.Time `json:"last_updated_at"`
+	} `jsob:"meta"`
+	Data map[string]struct {
+		Code  string  `json:"code"`
+		Value float64 `json:"value"`
+	} `json:"data"`
+}
+
 func Start() {
 	c := &http.Client{
 		Timeout: time.Second * time.Duration(config.Get.Timeout),
 	}
 
+	req, err := http.NewRequest(http.MethodGet, config.Get.CurrenciesHost, nil)
+	if err != nil {
+		// TODO handle error
+		log.Fatal(err)
+	}
+
+	req.Header.Set("apikey", config.Get.ApiKey)
+
 	for {
 		go func() {
 			start := time.Now()
 
-			path := config.Get.CurrenciesHost
-			res, err := c.Get(path)
+			httpRes, err := c.Do(req)
 			if err != nil {
 				// TODO handle error, save in db
 				// TODO handle timeout error
 				log.Fatal(err)
 			}
-			defer res.Body.Close()
+			defer httpRes.Body.Close()
 
 			responseTime := time.Since(start)
 
-			body, err := io.ReadAll(res.Body)
+			body, err := io.ReadAll(httpRes.Body)
 			if err != nil {
+				// TODO handle error, save in db
+				log.Fatal(err)
+			}
+
+			var res response
+			if err = json.Unmarshal(body, &res); err != nil {
 				// TODO handle error, save in db
 				log.Fatal(err)
 			}
@@ -38,7 +62,7 @@ func Start() {
 			// TODO parse into a struct
 			// TODO save in db
 
-			log.Println("body", string(body))
+			log.Println("response", res)
 			log.Println("response time", responseTime)
 
 		}()
